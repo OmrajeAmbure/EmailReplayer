213db8d6edb70595f02ec57b03d854089fcc43c1
OmrajeAmbure <omrajeambure27@gmail.com>
2025-07-12 20:20:33 +0530

    App running changes

--- email-writer/src/main/java/com/email_writer/EmailGeneratorController.java
+++ email-writer/src/main/java/com/email_writer/EmailGeneratorController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:5173/")
+@CrossOrigin("*")
 @RequestMapping("/api/email")
 @AllArgsConstructor
 public class EmailGeneratorController {
